d91ce51281224404bc72441fe495a0e760998a8c
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-03-14 14:57:25 +0100

    dotenv ok with Mongo URI

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+//
+//CORS / CORS MUST BE PLACE BEFORE THE MIDDLEWAIRES
+//
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, PATCH, OPTIONS"
+	);
+	next();
+});
+
+//
+//DATA BASE
+//
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => console.log("Connexion à MongoDB réussie !"))
+	.catch(() => console.log("Connexion à MongoDB échouée !"));
+
+//must be place before the JSON requests
+app.use(express.json());
+//
+//
+module.exports = app;
