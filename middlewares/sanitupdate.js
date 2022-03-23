cde875b2152aba0bebf89bd05e506429b0db7501
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-03-23 13:19:47 +0100

    update sanit ok

---
+++ middlewares/sanitupdate.js
@@ -0,0 +1,51 @@
+const Sauce = require('../models/sauce')
+
+module.exports = (req, res, next) => {
+	//
+	Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+		if (req.file && sauce.userId == req.auth.userId) {
+			let sauceObject = JSON.parse(req.body.sauce)
+			console.log('sauceObject : ', sauceObject)
+			console.log('req.body.sauce : ', req.body.sauce)
+			console.log('sauceObject.name :', sauceObject.name)
+			let maj = sauceObject.name.replace(/\$/g, 'chris')
+			sauceObject.name = maj
+			console.log('sauceObject.name updated :', sauceObject.name)
+			console.log(req.body.sauce.name)
+			req.body.sauce = JSON.stringify(sauceObject)
+			console.log(req.body.sauce)
+			//
+			next()
+		} else if (!req.file && sauce.userId == req.auth.userId) {
+			console.log(req.body)
+			let maj = req.body.name.replace(/\$/g, 'chris')
+			req.body.name = maj
+			console.log(req.body.name)
+			//req.body.sauce = JSON.stringify(sauceObject)
+			next()
+			//
+		}
+	})
+	//
+}
+
+/*
+module.exports = (req, res, next) => {
+	try {
+		let sauceObject = JSON.parse(req.body.sauce)
+		console.log('sauceObject.name :', sauceObject.name)
+		//
+		if (sauceObject.name == 'test') {
+			console.log('Le password ne répond pas aux critères de sécurité')
+			throw 'Le password ne répond pas aux critères de sécurité'
+		} else {
+			console.log('ok pass assez complexe')
+			next()
+		}
+	} catch {
+		res.status(401).json({
+			error: new Error('Message erreur'),
+		})
+	}
+}
+*/
--- routes/sauces.js
+++ routes/sauces.js
@@ -6,12 +6,13 @@ const auth = require('../middlewares/auth')
 const multer = require('../middlewares/multer-config')
 const sauceCtrl = require('../controllers/sauces')
 const sanit = require('../middlewares/sanit')
+const sanitupdate = require('../middlewares/sanitupdate')
 //const errorHandler = require("../middlewares/errorhandler.js");
 // !! auth before multer to prevent adding images from unidentified users
 router.post('/', auth, multer, sanit, sauceCtrl.createSauce)
 router.get('/', auth, sauceCtrl.getAllSauces)
 router.get('/:id', auth, sauceCtrl.getOneSauce)
-router.put('/:id', auth, multer, sauceCtrl.updateSauce)
+router.put('/:id', auth, multer, sanitupdate, sauceCtrl.updateSauce)
 router.delete('/:id', auth, sauceCtrl.deleteSauce)
 router.post('/:id/like', auth, sauceCtrl.likeSauce)
 //
