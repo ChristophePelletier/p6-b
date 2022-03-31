1b9ded6d93990561f9740a48b9d527a41f02a26c
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-03-31 10:11:40 +0200

    final

--- controllers/user.js
+++ controllers/user.js
@@ -70,7 +70,12 @@ exports.login = (req, res, next) => {
 						// 1: datas to endode in the token (payload)
 						// 2 : secret key
 						// 3 : time
+						/*
 						token: jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', {
+							expiresIn: '12h',
+						}),
+						*/
+						token: jwt.sign({ userId: user._id }, process.env.SEC, {
 							expiresIn: '12h',
 						}),
 						// OK Request headers : Bearer user._id crypted
